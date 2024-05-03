70de4174eb2025213ec451ff6b34b90f7c6f3e74
dima.14.12.2004.2015@gmail.com <dima.14.12.2004.2015@gmail.com>
2024-05-03 17:39:04 +0300

    fix: user_type in test db

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -14,10 +14,14 @@ int rc0 = sqlite3_exec(db, "DROP TABLE IF EXISTS users; DROP TABLE IF EXISTS use
 
 int rc2 = sqlite3_exec(db, "CREATE TABLE horses( id integer NOT NULL CONSTRAINT horses_pk PRIMARY KEY, name varchar(255), age integer, experience integer, owner varchar(255), price double ); CREATE TABLE jockeys( id integer NOT NULL CONSTRAINT jockeys_pk PRIMARY KEY, surname varchar(255), experience integer, year_of_birthday integer, address varchar(255) ); CREATE TABLE races( id integer NOT NULL CONSTRAINT races_pk PRIMARY KEY, date date, race_number integer, horse_id integer NOT NULL, jockey_id integer NOT NULL, taken_place integer, CONSTRAINT races_horses FOREIGN KEY(horse_id) REFERENCES horses(id), CONSTRAINT races_jockeys FOREIGN KEY(jockey_id) REFERENCES jockeys(id) ); CREATE TABLE user_type( id integer NOT NULL CONSTRAINT user_type_pk PRIMARY KEY, type varchar(255) NOT NULL ); CREATE TABLE users( id integer NOT NULL CONSTRAINT users_pk PRIMARY KEY, user_type integer NOT NULL, surname varchar(255) NOT NULL, password varchar(255) NOT NULL, CONSTRAINT users_user_type FOREIGN KEY(user_type) REFERENCES user_type(id) );", nullptr, nullptr, nullptr);
 
+int rc3 = sqlite3_exec(db, "INSERT INTO user_type (type) VALUES ('owner'), ('jockey'), ('admin');", nullptr, nullptr, nullptr);
+
+
 TEST_CASE("Init test db") 
 {
     REQUIRE(rc1 == SQLITE_OK);
     REQUIRE(rc2 == SQLITE_OK);
+    REQUIRE(rc3 == SQLITE_OK);
 }
 
 TEST_CASE("Test Authentification class") {
